196c8dbf300410696ffd44ed60a105f9c9e7827f
Ibrohim Ibrohimov <ibrohimtech.inbox@gmail.com>
2026-01-29 19:03:00 +0500

    Update products.js

--- js/products.js
+++ js/products.js
@@ -27,133 +27,140 @@ window.ZETT_PRODUCTS = [
   {
     key:'Astro', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/astro.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'Ballerina', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/ballerina.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'Builder', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/builder.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'CarPlay', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/carplay.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'DinoPop', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/dinopop.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'Excavator', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/excavator.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'Fairy', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/fairy.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'Galaxy', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/galaxy.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'Midnight', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/midnight.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'Minicars', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/minicars.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'MonsterTruck', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/monstertruck.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'Princess', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/princess.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'Raceway', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/raceway.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'RexRock', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/rexrock.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'RoarBlue', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/roarblue.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'SkyPilot', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/skypilot.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'Space', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/space.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'Speedster', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/speedster.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'Stone', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/stone.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'Traffic', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/traffic.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'Unicorn', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/unicorn.jpg',
     tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Echo leather', warranty:'12 months' }
+    specs:{ material:'Oxford Polyester 600D (Water-repellent)', size:'40 x 12 x 28',  warranty:'12 months' }
   },
 
   {
     key:'school_backpack', category:'teen_bags', price:'199,000 so\'m', image:'teen-bags/teen-bag1.png',
     images:['teen-bags/3.jpg', 'teen-bags/2.jpg', 'teen-bags/4.jpg', 'teen-bags/5.jpg', 'teen-bags/6.jpg', 'teen-bags/7.jpg', 'teen-bags/8.jpg',  ],
-    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
-    specs:{ material:'Substitute leather', warranty:'12 months' }
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'7-17 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Substitute leather', size:'40 x 12 x 28', warranty:'12 months' }
+  },
+
+  {
+    key:'school_backpack2', category:'teen_bags', price:'199,000 so\'m', image:'teen-bags/2.1.png',
+    images:['teen-bags/2.2.png', 'teen-bags/2.3.png', 'teen-bags/2.4.png', 'teen-bags/2.5.png', 'teen-bags/2.6.png'],
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'7-17 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Substitute leather', size:'40 x 12 x 28', warranty:'12 months' }
   }
 ];
