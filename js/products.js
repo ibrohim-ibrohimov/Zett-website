2ca3f9c9dde1b0da0c2eb37e3a09261c81e672c1
Ibrohim Ibrohimov <ibrohimtech.inbox@gmail.com>
2026-01-29 18:16:21 +0500

    Update products.js

--- js/products.js
+++ js/products.js
@@ -22,5 +22,138 @@ window.ZETT_PRODUCTS = [
     images:['bag.png', 'bag1.jpg', 'bag2.jpg' ],
     tagline:{ en:'Minimal carry for 15.6” devices', ru:'Минималистичная сумка для 15.6”', uz:'15.6” qurilmalar uchun sumka' },
     specs:{ material:'Echo leather', size:'15.6"', pockets:'Main + accessories', strap:'Padded shoulder', warranty:'12 months' }
+  },
+
+  {
+    key:'Astro', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/astro.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'Ballerina', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/ballerina.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'Builder', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/builder.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'CarPlay', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/carplay.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'DinoPop', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/dinopop.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'Excavator', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/excavator.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'Fairy', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/fairy.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'Galaxy', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/galaxy.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'Midnight', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/midnight.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'Minicars', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/minicars.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'MonsterTruck', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/monstertruck.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'Princess', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/princess.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'Raceway', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/raceway.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'RexRock', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/rexrock.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'RoarBlue', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/roarblue.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'SkyPilot', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/skypilot.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'Space', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/space.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'Speedster', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/speedster.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'Stone', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/stone.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'Traffic', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/traffic.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'Unicorn', category:'kids_bags', price:'99,000 so\'m', image:'kids-bags/unicorn.jpg',
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Echo leather', warranty:'12 months' }
+  },
+
+  {
+    key:'school_backpack', category:'teen_bags', price:'199,000 so\'m', image:'teen-bags/teen-bag1.png',
+    images:['teen-bags/3.jpg', 'teen-bags/2.jpg', 'teen-bags/4.jpg', 'teen-bags/5.jpg', 'teen-bags/6.jpg', 'teen-bags/7.jpg', 'teen-bags/8.jpg',  ],
+    tagline:{ en:'For 2-7 years olds', ru:'Для детей от 2 до 7 лет', uz:'2-7 yoshgacha bo\'lgan bolalar uchun' },
+    specs:{ material:'Substitute leather', warranty:'12 months' }
   }
 ];
